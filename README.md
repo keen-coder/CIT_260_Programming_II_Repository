2aa23af050d7d8494d1ac81e7c392017ac221a85
keen-coder <kknaur@gmail.com>
2025-08-13 14:50:48 -0400

    Added start files for assignment 00.

---
+++ README.md
@@ -0,0 +1,5 @@
+# CIT 260 Programming II Examples
+Code examples for my CIT-260 Programming II Course
+
+Most of these examples have either been developed by myself, or were taken from / adapted from
+Starting out with Python, 6th Edition, by Tony Gaddis.
---
+++ pa00_starter_files/160_review.py
@@ -0,0 +1,58 @@
+'''
+CIT160 REVIEW, Activity 0
+'''
+
+def main():
+    #call get_principle_investment
+    #call stairs
+    #call print_c_to_f
+    #call print_average_from_file
+
+    return
+
+
+'''
+# 1
+'''
+
+
+def get_principle_investment(*, future_value=0, rate=0.01, years=1):
+    #put your arithmetic here
+    return
+
+
+'''
+# 2
+'''
+
+
+def stairs():
+    #write your stairs code here
+
+    return
+
+
+'''
+# 3
+'''
+
+
+def print_c_to_f():
+    #Write the C to F conversion here
+
+    return
+
+
+'''
+#4
+'''
+
+
+def print_average_from_file():
+  #Write your code to open and calculate the average of the values in the file
+
+    return
+
+
+if __name__ == '__main__':
+    main()
